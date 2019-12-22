15942846c42cbee951f5a78bc576e09db49b88a5
Mullai <mullaimc@gmail.com>
2019-12-22 16:42:35 +0530

    removing unused methods

--- selenium-java/selenium-java/src/com/fb/basic/RunTest.java
+++ selenium-java/selenium-java/src/com/fb/basic/RunTest.java
@@ -7,25 +7,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.fb.utils.ReadPropertiesFile;
-import com.fb.utils.ScreenShot;
+import com.fb.utils.Utils;
 
 public class RunTest {
 	public static void main(String args[]) {
-		
-		String siteUrl ="";
-		String email ="";
-		String pwd ="";
-		String chromeDriverPath ="";
-		String chromeWebDriver ="";
-		String friendName ="";
+
+		String siteUrl = "";
+		String email = "";
+		String pwd = "";
+		String chromeDriverPath = "";
+		String chromeWebDriver = "";
+		String friendName = "";
 
 		try {
-			siteUrl = ReadPropertiesFile.getCredPropValue("SiteURL");
-			email = ReadPropertiesFile.getCredPropValue("TestUserEmail");
-			pwd = ReadPropertiesFile.getCredPropValue("TestUserPassword");
-			chromeWebDriver = ReadPropertiesFile.getConfigParm("ChromeWebDriver");
-			chromeDriverPath = ReadPropertiesFile.getConfigParm("ExecutableChromeDriverPath");
-			friendName = ReadPropertiesFile.getConfigParm("friendName");
+			siteUrl = ReadPropertiesFile.getPropValue("SiteURL");
+			email = ReadPropertiesFile.getPropValue("TestUserEmail");
+			pwd = ReadPropertiesFile.getPropValue("TestUserPassword");
+			chromeWebDriver = ReadPropertiesFile.getPropValue("ChromeWebDriver");
+			chromeDriverPath = ReadPropertiesFile.getPropValue("ExecutableChromeDriverPath");
+			friendName = ReadPropertiesFile.getPropValue("friendName");
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
@@ -36,20 +36,23 @@ public class RunTest {
 		WebDriver driver = new ChromeDriver(options);
 		FacebookLogin fl = new FacebookLogin();
 		UnFriend uf = new UnFriend();
-		ScreenShot sc = new ScreenShot();
-
-		driver.manage().window().maximize();
-		driver.get(siteUrl);
-
-		System.out.println("Login Test Case Passed : " +fl.validLogin(driver, email, pwd));
-		System.out.println("Unfriend Test Case Passed : " +uf.unFriendValid(driver, friendName));
-		
 		try {
-			sc.screen(driver);
+			Utils.screenShot(driver);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+		driver.manage().window().maximize();
+		driver.get(siteUrl);
+
+		System.out.println("Login Test Case Passed : " + fl.validLogin(driver, email, pwd));
+		System.out.println("Unfriend Test Case Passed : " + uf.unFriendValid(driver, friendName));
+
+		try {
+			Utils.screenShot(driver);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		driver.quit();
 	}
 }
